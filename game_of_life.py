2620c0b8775955324ab59460c90f6efb110fccf6
rugbyprof <terry.griffin@mwsu.edui>
2016-02-03 21:58:03 -0600

    cleaned up game of life

--- game_of_life.py
+++ game_of_life.py
@@ -1,13 +1,11 @@
 import time
 import os
 import random
-import platform
+import platform         # Used to determine if running on windows linux or osx
 
 
 class golBoard(object):
     """
-    NOT COMPLETE!
-
     An introduction to python using the game of life as a problem to solve in class.
     Not the most pythonic or succinct solution, but it's not meant to be.
 
@@ -25,12 +23,13 @@ class golBoard(object):
             self.currentGen = self.initRandGen(Density)
         else:
             self.currentGen = self.initGen()
+            
+        self.nextGen = self.initGen()
 
     def __str__(self):
         return "width:%d height:%d" % (self.width,self.height)      
 
         
-
     def makeAlive(self,row,col):
         """
         @function: makeAlive
@@ -39,23 +38,31 @@ class golBoard(object):
         @param: int y - Row to add life
         @returns: None
         """ 
-        self.currentGen[row-1][col-1] = True
+        self.currentGen[row-1][col-1] = 1
 
         
        
     def computeNextGen(self):
         """
         @function: computeNextGen
-        @description: Computes the next generation our cellular automata 
+        @description: Computes the next generation of our cellular automata 
         @param: None
         @returns: None
         """     
-        nextGen = self.initGen()
         for row in range(self.height):
             for col in range(self.width):
-                nextGen[row][col] = self.liveOrDie(row,col)
-        self.currentGen = nextGen
-
+                self.nextGen[row][col] = self.liveOrDie(row,col)
+    
+    def copyNextGen(self):
+        """
+        @function: copyNextGen
+        @description: Copies the next generation (after calculated) to our current generation  
+        @param: None
+        @returns: None
+        """  
+        for row in range(self.height):
+            for col in range(self.width):
+                self.currentGen[row][col] += self.nextGen[row][col] 
         
 
     def liveOrDie(self,r,c):
@@ -66,41 +73,35 @@ class golBoard(object):
         @param: int y - Row to check
         @returns: Int : 0 = nothing changes , -1 = dies , 1 = birth
         """    
-        neighbors = []
+
         alive = self.currentGen[r][c]
 
-        print(self.getNeighborWindow(r,c))
+        neighbors = self.getNeighborWindow(r,c)
+        
+        count = 0
+        
+        for i,j in neighbors:
+            count += self.currentGen[i][j]
 
-        # neighbors.append(self.currentGen[r-1][c-1])      # upper left
-        # neighbors.append(self.currentGen[r][c-1])        # upper middle   
-        # neighbors.append(self.currentGen[r+1][c-1])      # upper right
-        # neighbors.append(self.currentGen[r+1][c])        # right
-        # neighbors.append(self.currentGen[r-1][c])        # left 
-        # neighbors.append(self.currentGen[r-1][c+1])      # bottom left
-        # neighbors.append(self.currentGen[r][c+1])        # bottom middle 
-        # neighbors.append(self.currentGen[r+1][c+1])      # bottom right
-            
-        # count = neighbors.count(True)
-
-        # if(alive):
-        #     if count < 2 or count > 3:
-        #         return 0 
-        #     else:
-        #         return 1
-        # else:
-        #     if count == 3:
-        #         return 1
-        #     else:
-        #         return 0 
+        if(alive):
+            if count < 2 or count > 3:
+                return -1 
+            else:
+                return 0
+        else:
+            if count == 3:
+                return 1
+            else:
+                return 0 
         
  
     def getNeighborWindow(self,r,c):
         """
-        @function: liveOrDie
-        @description: Calculates whether a cell lives or dies based on Game of Life rules
-        @param: int x - Column to check
-        @param: int y - Row to check
-        @returns: Int : 0 = nothing changes , -1 = dies , 1 = birth
+        @function: getNeighborWindow
+        @description: Gets the neighbors of a cell 
+        @param: int r - Row to check
+        @param: int c - Col to check
+        @returns: List : List neighboring tuples (e.g. [(1,1),(1,2),(1,3)] ) 
         """     
         neighbors = []
         srow = r - 1
@@ -118,8 +119,8 @@ class golBoard(object):
         if erow >= self.height:
             erow = self.height - 1
 
-        if (ecol + 1) >= self.width:
-            ecol = self.width
+        if ecol >= self.width:
+            ecol = self.width-1
 
         for i in range(srow,erow+1):
             for j in range(scol,ecol+1):
@@ -136,7 +137,7 @@ class golBoard(object):
         @function: initGen
         @description: Initializes a single generation 
         @param: None
-        @returns: list - 2D list containing False
+        @returns: list - 2D list containing zeros
         """     
         #return [[False] * self.width for row in range(self.height)]
         
@@ -152,7 +153,7 @@ class golBoard(object):
         @function: initRandGen
         @description: Initializes a random generation 
         @param: float - density (how many lives to create)
-        @returns: list - 2D list containing False and True
+        @returns: list - 2D list containing zeros and ones
         """     
         gen = self.initGen()
         
@@ -161,7 +162,7 @@ class golBoard(object):
         for i in range(numberOfLives):
                 row = random.randint(0, self.height-1)
                 col = random.randint(0, self.width-1)
-                gen[row][col] = self.randomLife()       # ??
+                gen[row][col] = self.randomLife()
         return gen
         
 
@@ -170,72 +171,79 @@ class golBoard(object):
         @function: randomLife
         @description: Generates a random life (zero or one)
         @param: none
-        @returns: bool - zero or one (alive or dead)
+        @returns: int - zero or one (alive or dead)
         """    
         if random.random() > .5:
-            x = True
+            x = 1
         else:
-            x = False
+            x = 0
         return x
 
 
-    def stringifyWorld(self):
+    def stringifyWorld(self,current=True):
         """
         @function: stringifyWorld
-        @description: Creates a string version of the 2D list representing our world
+        @description: Creates a string version of the 2D list representing our world with colors!
         @param: none
-        @returns: string - a string version 
-        """    
-        count = 0
-        format = ';'.join([str(0), str(32), str(40)])
-        string = "\n\n"
-        string += '  '
-        for i in range(self.width):
-            string += ' '+str(i%10)
-        string += "\n"
-        for row in self.currentGen:
-            string += str(count % 10)+' '
-            count += 1
-            for cell in row:
-                if cell == False:
-                    string += '\x1b[%sm%s\x1b[0m' % (format,' .')
-                else:
-                    string += ' x'
-            string += "\n"
-        return string
+        @returns: string - a string version of our world
+        """
         
+        """ Unicode characters makes it Sloooooooow"""
+        chars = {
+            -1:u' \u2689',
+            0:"  ",
+            1:u' \u2689'
+        }
+        
+        """ not really used either, used colored backgrounds instead """
+        chars = {
+            -1:" X",
+            0:" .",
+            1:" X"
+        }
+        
+        
+        colors = {
+            "dead": ';'.join([str(0), str(31), str(41)]),
+            "nada": ';'.join([str(3), str(37), str(47)]),
+            "live": ';'.join([str(0), str(30), str(40)]),
+            "new" : ';'.join([str(0), str(32), str(42)])
+        }
+        
+        string = "\n"
 
-    def printColoredSection(self,x,y):
-        """
-        @function: printColoredSection
-        @description: Prints a colored portion of the world
-        @param: none
-        @returns: none
-        """
         
-        clearScreen()
-        format = ';'.join([str(0), str(32), str(40)])
-        count = 0
-        string = "\n\n"
-        string += '  '
-        for i in range(self.width):
-            string += ' '+str(i%10)
-        string += "\n"
-        for row in self.currentGen:
-            string += str(count % 10)+' '
-            count += 1
-            for cell in row:
-                if cell == False:
-                    string += '\x1b[%sm%s\x1b[0m' % (format,' .')
-                else:
-                    string += ' x'
-            string += "\n"
-        print(string)      
-        
-        
-    def printDebug(self):
-        for row in self.currentGen:  
-            print(row)
+        if current == True:
+            for row in self.currentGen:
+                for cell in row:
+                    if cell == 0:
+                        format = colors["nada"]
+                        string += '\x1b[%sm%s\x1b[0m' % (format,chars[0])
+                    else:
+                        format = colors["live"]
+                        string += '\x1b[%sm%s\x1b[0m' % (format,chars[1])
+                string += "\n"
+            return string 
+        else:
+            for r in range(self.height):
+                for c in range(self.width):
+                    nval = self.nextGen[r][c]
+                    cval = self.currentGen[r][c]
+                    
+                    if nval == -1:
+                        format = colors["dead"]
+                        string += '\x1b[%sm%s\x1b[0m' % (format,' X')
+                    elif nval == 1:
+                        format = colors["new"]
+                        string += '\x1b[%sm%s\x1b[0m' % (format,' X')
+                    else:
+                        format = colors["nada"]
+                        string += '\x1b[%sm%s\x1b[0m' % (format,chars[cval])      
+                                          
+                string += "\n"
+                
+            return string 
+     
               
 
 def clearScreen():
@@ -275,29 +283,58 @@ def print_format_table():
         for i in range(self.height):
             for j in range(self.width):
                 print self.currentGen[i][j]
+                
+def getTerminalSize():
+    import os
+    env = os.environ
+    def ioctl_GWINSZ(fd):
+        try:
+            import fcntl, termios, struct, os
+            cr = struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ,
+        '1234'))
+        except:
+            return
+        return cr
+    cr = ioctl_GWINSZ(0) or ioctl_GWINSZ(1) or ioctl_GWINSZ(2)
+    if not cr:
+        try:
+            fd = os.open(os.ctermid(), os.O_RDONLY)
+            cr = ioctl_GWINSZ(fd)
+            os.close(fd)
+        except:
+            pass
+    if not cr:
+        cr = (env.get('LINES', 25), env.get('COLUMNS', 80))
+
+        ### Use get(key[, default]) instead of a try/catch
+        #try:
+        #    cr = (env['LINES'], env['COLUMNS'])
+        #except:
+        #    cr = (25, 80)
+    return int(cr[1]), int(cr[0])
 
 
-rows = 20
-cols = 30
+x,y = getTerminalSize()
+
+rows = int(y * .95)
+cols = int(x * .48)
+
 generations = 30
 density = .25
 sleep = .05
-# for x in range(generations):
-#     b = golBoard(rows,cols,True,density)
-#     clearScreen()
-#     print(b.stringifyWorld())
-#     time.sleep(sleep)
-
-b.printColoredSection(5,6)
-print(platform.system())
-
-#print_format_table()
-
-# for i in range(10):
-#     r = random.randint(0,rows-1)
-#     c = random.randint(0,cols-1)
-#     print('*',r,c)
-#     b.liveOrDie(r,c) 
 
 
-#print_format_table()
+b = golBoard(rows,cols,True,density)
+
+
+for i in range(1000):
+    clearScreen()
+    print(b.stringifyWorld(True))
+    b.computeNextGen()
+   # time.sleep(1)
+    #clearScreen()
+    #print(b.stringifyWorld(False))
+    b.copyNextGen()
+    time.sleep(.01)
+    
+# print_format_table()
