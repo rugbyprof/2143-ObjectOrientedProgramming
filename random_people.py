9b54440135839deebc8f3e03798275fef0642425
rugbyprof <terry.griffin@mwsu.edui>
2016-03-01 10:49:27 -0600

    adding random people file

---
+++ random_people.py
@@ -0,0 +1,27 @@
+import json
+import requests
+
+people = []
+
+for i in range(100):
+    response = requests.get('http://api.randomuser.me/?nat=us&results=1000')
+    data = response.json()
+    print(i)
+
+    people.extend(data['results'])
+
+fp = open('random_people.json', 'w')
+print(len(people))
+
+fp.write(json.dumps(people))
+
+# with open('people.json') as data_file:    
+#     data = json.load(data_file)
+
+# pp = pprint.PrettyPrinter(depth=4)
+# pp.pprint(data['results'])
+
+# print(len(data['results']))
+# for person in data['results']:
+#     pp.pprint(person['user'])
+#     print()
