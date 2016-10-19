5ee331d14d10fbd80ae5efb7d104d5835e99606a
Terry Griffin <rugbyprof@users.noreply.github.com>
2016-10-19 14:08:19 -0500

    Update game_starter.py

--- Assignments/Program-02/game_starter.py
+++ Assignments/Program-02/game_starter.py
@@ -82,12 +82,12 @@ class Player(object):
     @Returns: None
     """
     def AddOpponents(self,opponent):
-        if not type(opponent) == list and not opponent.Name == self.Name:
-            self.Opponents[opponent.Name] = opponent
-        else:
-            for op in opponent:
-                if not op.Name == self.Name:
-                    self.Opponents[op.Name] = op
+        # if not type(opponent) == list and not opponent.Name == self.Name:
+        #     self.Opponents[opponent.Name] = opponent
+        # else:
+        for op in opponent:
+            if not op.Name == self.Name:
+                self.Opponents[op.Name] = op
 
     """
     @Method: __str__
@@ -156,7 +156,7 @@ class Player(object):
     def RandomRoll(self):
         Score = 0
         NumRolls = 0
-        for i in range(random.randint(1,7)):
+        for i in range(random.randint(1,self.Strategies['Random'])):
             NumRolls += 1
             roll = self.pig.Roll()
             if roll == 0:
