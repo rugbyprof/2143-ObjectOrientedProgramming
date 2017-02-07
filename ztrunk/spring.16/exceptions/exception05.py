215aa8e257776e2f84d62dc856e135e01a62addd
Terry Griffin <rugbyprof@users.noreply.github.com>
2017-02-07 10:02:21 -0600

    Update exception05.py

--- ztrunk/spring.16/exceptions/exception05.py
+++ ztrunk/spring.16/exceptions/exception05.py
@@ -1,5 +1,6 @@
 """
- We handle exceptions by wrapping any code that might throw one (whether it is exception code itself, or a call to any function or method that may have an exception raised inside it) inside a try...except clause.
+ We handle exceptions by wrapping any code that might throw one (whether it is exception code itself, or a call to 
+ any function or method that may have an exception raised inside it) inside a try...except clause.
  
  Catches ANY type of exception
  """
