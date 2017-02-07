606fd5f3a66b8e391f832c2437f437b8b3b0dd12
Terry Griffin <rugbyprof@users.noreply.github.com>
2017-02-07 10:04:12 -0600

    Update exception06.py

--- ztrunk/spring.16/exceptions/exception06.py
+++ ztrunk/spring.16/exceptions/exception06.py
@@ -1,5 +1,5 @@
 """
-The problem with the above code is that it will catch any type of exception.
+The problem with the previous example is that it will catch any type of exception.
 What if we were writing some code that could raise both a TypeError and a ZeroDivisionError? 
 We might want to catch the ZeroDivisionError, but let the TypeError propagate to the console. 
 """
@@ -10,6 +10,6 @@ def funny_division(anumber):
        except ZeroDivisionError:
            return "Silly wabbit, you can't divide by zero!"
 
-#print(funny_division(0))#
-#print(funny_division(50.0))
+print(funny_division(0))#
+print(funny_division(50.0))
 print(funny_division("hello"))
