b0b2dc783a2982e873ce52aa7625b191c495c6c8
Terry Griffin <rugbyprof@users.noreply.github.com>
2017-04-11 10:27:41 -0500

    Update exception01.py

--- Resources/exceptions/exception01.py
+++ Resources/exceptions/exception01.py
@@ -11,3 +11,11 @@ lst.add
 d = {'a': 'hello'}
 d['b']
 print(this_is_not_a_var)
+
+"""
+hello world
+Traceback (most recent call last):
+  File "ex.py", line 2, in <module>
+    x = 5 / 0
+ZeroDivisionError: integer division or modulo by zero
+"""
